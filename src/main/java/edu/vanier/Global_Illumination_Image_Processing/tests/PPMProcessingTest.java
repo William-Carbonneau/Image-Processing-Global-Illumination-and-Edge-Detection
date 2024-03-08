5edc1422cd04b47ef2ab22dc2a31293fc04d6acb
Loovdrish Sujore <loovdrish.sujore@gmail.com>
2024-03-08 09:41:08 -0500

    Bitmap changes made
    The description of color works with 8-bits

--- src/main/java/edu/vanier/Global_Illumination_Image_Processing/tests/PPMProcessingTest.java
+++ src/main/java/edu/vanier/Global_Illumination_Image_Processing/tests/PPMProcessingTest.java
@@ -21,45 +21,22 @@ public class PPMProcessingTest {
         File file = new File("src\\main\\resources\\images\\landscape.bmp");
         BufferedImage img = ImageIO.read(new File(file.getAbsolutePath()));
         // you should stop here
-        int[][] green = new int[480][360];
-        int[][] alpha = new int[480][360];
-        int[][] red = new int[480][360];
-        int[][] blue = new int[480][360];
+        byte[][] green = new byte[480][360];
+        byte[][] alpha = new byte[480][360];
+        byte[][] red = new byte[480][360];
+        byte[][] blue = new byte[480][360];
         for(int x=0; x<480; x++){
             for(int y=0; y<360; y++){
             int color = img.getRGB(x,y);
-            alpha[x][y] = (int)(color>>24);
-            red[x][y] = (int)(color>>16);
-            green[x][y] = (int)(color>>8);
-            blue[x][y] = (int)(color);
-            }
-        }
-        int[][] result = new int[480][360];
-        for(int x=0; x<480; x++){
-            for(int y=0; y<360; y++){
-                //Color color = new Color(red[x][y], green[x][y], blue[x][y], alpha[x][y]);
+            //red[x][y] = (byte)(color>>16);
+            green[x][y] = (byte)(color>>8);
+            //blue[x][y] = (byte)(color);
+            //System.out.println(red[x][y]);
+            System.out.println("color "+ color);
+            System.out.println(green[x][y]);
+            //System.out.println(blue[x][y]);
             }
         }
         
-        
-    }
-        public static  int[] loadData(String filename) throws FileNotFoundException {
-        File foo = new File(filename);
-        Scanner scan = new Scanner(foo);
-            scan.nextLine();
-            scan.nextLine();
-            int width = scan.nextInt();
-            int height = scan.nextInt();
-            System.out.print(height);
-            scan.nextLine();
-            int[] array = new int [width * height];
-            int i = 0;
-            while (scan.hasNextInt()) {
-                array[i] = scan.nextInt();
-                i++;
-                System.out.print("yes");
-                }
-            scan.close();
-        return array;
     }
 }
