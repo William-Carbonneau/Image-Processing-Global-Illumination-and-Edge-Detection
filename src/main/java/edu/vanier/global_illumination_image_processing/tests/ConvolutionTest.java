80a53229e8b7512d4a26a9659d4a1c246745cf36
Loovdrish Sujore <loovdrish.sujore@gmail.com>
2024-03-11 21:44:43 -0400

    progress made

--- src/main/java/edu/vanier/global_illumination_image_processing/tests/ConvolutionTest.java
+++ src/main/java/edu/vanier/global_illumination_image_processing/tests/ConvolutionTest.java
@@ -1,23 +1,33 @@
 package edu.vanier.Global_Illumination_Image_Processing.tests;
 
-import edu.vanier.Global_Illumination_Image_Processing.models.Convolution;
+import edu.vanier.global_illumination_image_processing.controllers.FXMLConvolutionController;
+import edu.vanier.global_illumination_image_processing.controllers.FXMLMainAppController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 
 /**
  * Test Class for Convolution
  */
-public class ConvolutionTest {
+public class ConvolutionTest extends Application{
     public static void main(String[] args) {
-        float[][] gauss = {
-            {1,2,1},
-            {2,4,2},
-            {1,2,1}
-        };
-        float[][] matrix={
-            {1,2,1,0,1,1,1},
-            {1,1,5,0,1,1,1},
-            {1,1,0,0,1,1,1}
-        };
-        float[][] result = Convolution.performConvolutionOnImage(gauss, matrix);
-        Convolution.print2DArray(result);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/FXMLConvolutionsScene.fxml"));
+        loader.setController(new FXMLConvolutionController(primaryStage));
+        Pane root = loader.load();
+        //-- 2) Create and set the scene to the stage.
+        Scene scene = new Scene(root, 1000, 700);
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        // We just need to bring the main window to front.
+        primaryStage.setAlwaysOnTop(true);            
+        primaryStage.show();
     }
 }
