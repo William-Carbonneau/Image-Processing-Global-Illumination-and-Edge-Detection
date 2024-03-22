f6b5e4804652792fe4d8178b0b1b23c0e6042173
William-Carbonneau <2265724@edu.vaniercollege.qc.ca>
2024-03-22 09:49:09 -0400

    modify render FXML controller

--- src/main/java/edu/vanier/global_illumination_image_processing/controllers/FXMLRenderSceneController.java
+++ src/main/java/edu/vanier/global_illumination_image_processing/controllers/FXMLRenderSceneController.java
@@ -1,25 +1,32 @@
 package edu.vanier.global_illumination_image_processing.controllers;
 
+import edu.vanier.global_illumination_image_processing.rendering.Scene;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 /**
- *
- * @author Zachary Tremblay
+ * The controller for the rendering scene
+ * 
+ * @author Zachary Tremblay and Willima Carbonneau
  */
 public class FXMLRenderSceneController {
-    @FXML
-    VBox RootVBox;
-    @FXML
-    MenuItem BackToTitleMenuItem;
-    @FXML
+    @FXML VBox RootVBox;
+    @FXML MenuItem BackToTitleMenuItem;
+    @FXML ListView listObjectList;
+    
     public void initialize(){
+        // create the render scene
+        Scene mainScene = new Scene();
+        // create list of elements - needs a new class wrapper
+        //ObservableList<ObjWrapper>
         
         BackToTitleMenuItem.setOnAction((event)->{
             try {
@@ -33,6 +40,12 @@ public class FXMLRenderSceneController {
             }
         });
         
+//        listObjectList.setOnMouseClicked();
+        
+    }
+    
+    class ObjWrapper {
+    
     }
     
 }
